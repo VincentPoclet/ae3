9c2590f5ec0c64e6285fa47d5c5f3d4048d90ae6
Unknown <loic.poire@orange.fr>
2017-03-22 15:23:29 +0100

    Event.update + Event.delete
    Ajout des fonctions de modification et de suppression des événements

--- api/controllers/EventsController.js
+++ api/controllers/EventsController.js
@@ -47,5 +47,46 @@ module.exports = {
 				return res.status(200).json({'err': null, 'data': row});
 			}
 		});
+	},
+
+	delete: function(req, res) {
+		Events.destroy({
+			longEvent: req.param.longEvent,
+			lattEvent: req.param.lattEvent,
+		}).exec(function(err, row) {
+			if (err) {
+				return res.status(500).json({'err': err.message, 'data': null});
+			}
+			if (!row) {
+				return res.status(400).json({'err': 'No events found according to criterias', 'data': null});
+			}
+			return res.status(200).json({'err': null, 'data': row});
+		});
+	},
+
+	update: function(req, res) {
+		Events.update({
+			longEvent: req.param.long,
+			lattEvent: req.param.latt
+		}, {
+			nomEvent: req.param('nomEvent'),
+			adresse: req.param('adresse'),
+			codePostal: req.param('codePostal'),
+			ville: req.param('ville'),
+			dateDebut: req.param('dateDebut'),
+			dateFin: req.param('dateFin'),
+			typeEvent: req.param('typeEvent'),
+			descEvent: req.param('descEvent'),
+			longEvent: req.param('longEvent'),
+			lattEvent: req.param('lattEvent')
+		}).exec(function(err, row) {
+			if (err) {
+				return res.status(500).json({'err': err, 'data': null});
+			}
+			if (!row) {
+				return res.status(400).json('err': 'No events found according to criterias', 'data': null);
+			}
+			return res.status(200).json('err': null, 'data': row);
+		})
 	}
 };
