1d5ce3d798d7c659fc679a5205b4c83288f3e145
Unknown <loic.poire@orange.fr>
2017-05-16 16:33:57 +0200

    Correction plannings
    Avec des console.log, oui monsieur

--- api/controllers/PlanningController.js
+++ api/controllers/PlanningController.js
@@ -51,34 +51,47 @@ module.exports = {
 
 	},
 	select: function(req, res) {
+		console.log("Start");
 		var result = {};
 		var nbPlan = 1;
 		var cmptPl = 0;
+		console.log("Initialisation terminée");
 		Planning.find({
 			user: req.param('idUser')
 		}).exec(function(err, row) {
+			console.log("Exécution du find terminée");
 			if (err) {
+				console.log("Erreur !");
 				return res.status(500).json({'err': err, 'data': row});
 			}
 			if (!row) {
+				console.log("Ligne vide !");
 				return res.status(400).json({'err': 'No planning found', 'data': data});
 			}
 			result.plannings = row;
 			nbPlan = row.length;
+			console.log("Parcours des " + nbPlan + " plannings");
 			result.plannings.forEach(function(el, i) {
 				cmptPl++;
+				console.log("Planning numéro " + cmptPl);
 				PlannedEvent.find({
 					planning: el.id
 				}).exec(function(err, row) {
+					console.log("Exécution du find terminée");
 					if (err) {
+						console.log("Erreur !");
 						return res.status(500).json({'err': err, 'data': row});
 					}
+					console.log("Affectation");
 					result.plannings[i].events = row;
 					// if (cmptPl == nbPlan) {
 					// 	return res.status(200).json({'err': null, 'data': result});	
 					// }
+					console.log("Planning traité");
 				});
 			});
+			console.log(result);
+			console.log("Envoi des données :");
 			return res.status(200).json({'err': null, 'data': result});
 		});
 	}
